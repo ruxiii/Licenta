e479034fcba9e9dd18e302c7393e87639b0fe87d
ruxiii <ruxandra.iftimi@gmail.com>
2024-03-08 15:08:52 +0200

    @Crossorigin + header in frontend

--- backend/src/main/java/org/example/licenta/controllers/MapController.java
+++ backend/src/main/java/org/example/licenta/controllers/MapController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class MapController {
 
     @Autowired
